0bc959215b557e2faba78e07b4225d2d0eb9a14b
Edison Vallejo <edavidvallejo@gmail.com>
2017-05-03 14:48:23 -0700

    Adding Log messages

--- RulerLess/app/src/main/java/csm117/rulerless/MainActivity.java
+++ RulerLess/app/src/main/java/csm117/rulerless/MainActivity.java
@@ -4,18 +4,22 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     LocationListener locListen;
     TextView display;
     Location currentLoc;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "Application Started");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         display = (TextView) findViewById(R.id.distText);
