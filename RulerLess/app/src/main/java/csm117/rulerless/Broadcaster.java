9206b32395d7163b2fb4c51fa46d0d2235e89d12
kwong623 <kelvinwong623@gmail.com>
2017-05-10 15:18:52 -0700

    Added updates Broadcaster

--- RulerLess/app/src/main/java/csm117/rulerless/Broadcaster.java
+++ RulerLess/app/src/main/java/csm117/rulerless/Broadcaster.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
 
 /**
  * Created by Kelvin Wong on 5/10/2017.
@@ -29,6 +30,14 @@ public class Broadcaster  extends BroadcastReceiver {
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             // Check to see if Wi-Fi is enabled and notify appropriate activity
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi P2P is enabled
+                Log.d("test", "onReceive: WifiP2P enabled");
+            } else {
+                // Wi-Fi P2P is not enabled
+                Log.d("test", "onReceive: WifiP2P not enabled");
+            }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             // Call WifiP2pManager.requestPeers() to get a list of current peers
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
